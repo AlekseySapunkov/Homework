98fe07cd042fc218b2b7a4549b4cd3982d05f826
Sapunkov Aleksey <88789118+AlekseySapunkov@users.noreply.github.com>
2025-01-26 10:38:08 +0300

    add method shofSedesOfTriangle

--- src/App.java
+++ src/App.java
@@ -1,8 +1,10 @@
 public class App {
     static Point point = new Point(1,2);
+    static Triangle triangle = new Triangle(1, 2, 3);
         public static void main(String[] args) throws Exception {
             point.showCoordinates();
             point.findDistanceFromTheOrigin();
             point.moveToVector(1,2);
+            triangle.showSidesOfTriangle();
     }
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -8,4 +8,10 @@ public class Triangle {
         c= thirsd;
 
     }
+    public void showSidesOfTriangle(){
+        System.out.println("Sides of triangle:");
+        System.out.println("a= "+a);
+        System.out.println("b= "+b);
+        System.out.println("c= "+c);
+    }
 }
